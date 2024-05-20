59ad12c4636b248a5497be39e54e1ea3b3bd47c1
ParisiBR <parisi18@unifesp.br>
2024-05-20 19:37:21 -0300

    initial setup

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for setup project.
+URL configuration for todolist project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
