78ca7478f46571bac436549c59ec5a0e67a627ed
ParisiBR <parisi18@unifesp.br>
2024-05-21 17:56:29 -0300

    static files

--- galery/urls.py
+++ galery/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from galery.views import index
+from galery.views import index, image
 
 urlpatterns = [
     path('', index),
+    path('image/', image)
 ]
--- galery/views.py
+++ galery/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'galery/index.html')
+
+def image(request):
+    return render(request, 'galery/image.html')
