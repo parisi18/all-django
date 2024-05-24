beed78f4493d8b592de9b55ea77795ecc8665719
ParisiBR <parisi18@unifesp.br>
2024-05-23 22:13:43 -0300

    Fotografia return

--- galery/models.py
+++ galery/models.py
@@ -17,5 +17,6 @@ class Fotografia(models.Model):
     foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+
     def __str__(self):
-        return f"Fotografia [nome={self.nome}]"
+        return self.nome
