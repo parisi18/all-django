ae7fa15af73b767edbda37c5ed8ec34da4eadfc0
ParisiBR <parisi18@unifesp.br>
2024-06-10 11:49:54 -0300

    Login action

--- users/views.py
+++ users/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render, redirect
 from users.forms import LoginForm, SubscribeForm
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 def login(request):
     form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form['username'].value()
+            password = form['password'].value()
+
+        user = auth.authenticate(
+            request,
+            username=username, 
+            password=password
+        )
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            return redirect('login')
+
+
     return render(request, 'users/login.html', {'form': form})
 
 def subscribe(request):
