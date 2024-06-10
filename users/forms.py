be2f8f091a9a346976a8077c7825f69e5185131f
ParisiBR <parisi18@unifesp.br>
2024-06-10 11:04:49 -0300

    Subscribe form

--- users/forms.py
+++ users/forms.py
@@ -19,3 +19,41 @@ class LoginForm(forms.Form):
                    'placeholder': 'Password'}
         )
     )
+
+class SubscribeForm(forms.Form):
+    username = forms.CharField(
+        label='Username', 
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Username'},
+        )
+    )
+
+    email = forms.EmailField(
+        label='Email', 
+        required=True,
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Email'}
+        )
+    )
+
+    password = forms.CharField(
+        label='Password', 
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Type your password'}
+        )
+    )
+
+    password_confirm = forms.CharField(
+        label='Password confirm', 
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Type your password again'}
+        )
+    )
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from users.forms import LoginForm
+from users.forms import LoginForm, SubscribeForm
 
 def login(request):
     form = LoginForm()
     return render(request, 'users/login.html', {'form': form})
 
 def subscribe(request):
-    return render(request, 'users/subscribe.html')
+    form = SubscribeForm()
+    return render(request, 'users/subscribe.html', {'form': form})
