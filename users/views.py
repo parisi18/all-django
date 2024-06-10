07e0835d94dae78187438921e87ac545b3a17120
ParisiBR <parisi18@unifesp.br>
2024-06-10 11:43:13 -0300

    Subscribe action

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from users.forms import LoginForm, SubscribeForm
+from django.contrib.auth.models import User
 
 def login(request):
     form = LoginForm()
@@ -7,4 +8,24 @@ def login(request):
 
 def subscribe(request):
     form = SubscribeForm()
+
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+
+        if form.is_valid():
+            if form["password"].value() != form["password_confirm"].value():
+                return redirect('subscribe')
+        
+            username = form['username'].value()
+            email = form['email'].value()
+            password = form['password'].value()
+
+            if(User.objects.filter(username=username).exists()):
+                return redirect('subscribe')
+            
+            user = User.objects.create_user(username, email, password)
+            user.save()
+
+            return redirect('login')
+            
     return render(request, 'users/subscribe.html', {'form': form})
