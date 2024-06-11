fc1e72460dc19d01d291ec18c16b071007b45e23
ParisiBR <parisi18@unifesp.br>
2024-06-11 15:11:06 -0300

    User login conditional

--- galery/views.py
+++ galery/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Fotografia
-
+from users.views import login
+from django.contrib import messages
 
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'You must be logged in to access this page')
+        return redirect('login')
+
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True) 
     return render(request, 'galery/index.html', {"cards": fotografias})
 
@@ -11,7 +16,12 @@ def imagem(request, foto_id):
     return render(request, 'galery/imagem.html', {"fotografia": fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'You must be logged in to access this page')
+        return redirect('login')
+    
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+
     if("buscar" in request.GET):
         nome_a_buscar = request.GET["buscar"]
         if(nome_a_buscar):
