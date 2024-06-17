1cefb6e79d3094feef47f1c3aa371b5eaa3768ab
ParisiBR <parisi18@unifesp.br>
2024-06-16 21:24:58 -0300

    aws bucket config

--- setup/settings.py
+++ setup/settings.py
@@ -19,7 +19,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.galery.apps.GaleryConfig',
     'apps.users.apps.UsersConfig',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -117,11 +117,30 @@ USE_I18N = True
 
 USE_TZ = True
 
+# AWS Config
+
+AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS'))
+AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET'))
+AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_BUCKET'))
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_DEFAULT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION  = 'static'
+AWS_QUERYSTRING_AUTH = False
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'setup/static')
@@ -133,7 +152,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -146,3 +165,4 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     messages.SUCCESS: 'success',
 }
+
